e640d2cd51bf6802cbb94d009a7c45e357df9e55
geneh96 <geneh96@gmail.com>
2017-01-30 14:10:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gene/week3_demo1/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.gene.week3_demo1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btn = (Button)findViewById(R.id.btn_activity2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+            }
+        });
+    }
+    /*public void m0nClick(View view) {
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }*/
+}
---
+++ app/src/main/java/com/example/gene/week3_demo1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.gene.week3_demo1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.LoginFilter;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("ACTIVITY 1 TAG", "OnCreate method is invoked");
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ACTIVITY 1 TAG", "OnStart method is invoked");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ACTIVITY 1 TAG", "OnResume method is invoked");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ACTIVITY 1 TAG", "OnPause method is invoked");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ACTIVITY 1 TAG", "OnStop method is invoked");
+
+    }
+
+    public void m0nClick(View view) {
+
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
